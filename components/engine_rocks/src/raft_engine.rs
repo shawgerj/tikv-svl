faf0287346e45065e05fe8979e19eeae1a95b0cf
shawgerj <shawgerj@cs.wisc.edu>
2025-01-11 01:48:49 +0000

    drafting new garbage collector. Incomplete now

--- components/engine_rocks/src/raft_engine.rs
+++ components/engine_rocks/src/raft_engine.rs
@@ -154,35 +154,132 @@ impl RaftEngineReadOnly for RocksEngine {
     }
 }
 impl RocksEngine {
+    fn gc_eligible(key: &[u8], end: u64) -> bool {
+	let (region_id, index) = keys::decode_raft_log_key(key);
+	index < end
+    }
+    
+    // shawgerj: rather than seek and scan in Raft-LSM, we need to seek and scan the log. 
     fn gc_impl(
         &self,
         raft_group_id: u64,
         mut from: u64,
-        to: u64,
+        to: u64, // equals persisted I_app due to prior sync
         raft_wb: &mut RocksWriteBatch,
     ) -> Result<usize> {
-        if from == 0 {
-            let start_key = keys::raft_log_key(raft_group_id, 0);
-            let prefix = keys::raft_log_prefix(raft_group_id);
-            match self.seek(&start_key)? {
-                Some((k, _)) if k.starts_with(&prefix) => from = box_try!(keys::raft_log_index(&k)),
-                // No need to gc.
-                _ => return Ok(0),
-            }
-        }
-        if from >= to {
-            return Ok(0);
-        }
+	let logtail = self.get_cf(CF_DEFAULT, "wotr_tail").unwrap();
+	let vtail = kv.get_cf(CF_DEFAULT, "vtail").unwrap();
+	let new_vtail = vtail;
+	
+	let iter = self.get_wotr().wotr_iter_init().unwrap();
+	let _ = iter.seek(logtail);
 
-        for idx in from..to {
-            let key = keys::raft_log_key(raft_group_id, idx);
-            raft_wb.delete(&key)?;
-            if raft_wb.count() >= Self::WRITE_BATCH_MAX_KEYS * 2 {
-                raft_wb.write()?;
-                raft_wb.clear();
-            }
-        }
-        Ok((to - from) as usize)
+	let prefix = keys::raft_log_prefix(raft_group_id);
+
+	while (iter.valid().unwrap() > 0 && iter.position() < vtail {
+	    let key = unsafe {
+		slice::from_raw_parts(iter.key().unwrap(),
+				      iter.key_size().unwrap() as usize)
+	    };
+
+	    let value = unsafe {
+		slice::from_raw_parts(iter.value().unwrap(),
+				      iter.value_size().unwrap() as usize)
+	    };
+
+	    // not a raft entry: write the k-v again at log head
+	    if !keys::is_raft_key(&key) {
+		raft_wb.put(key, value);
+	    }
+	    
+	    // if raft key can't be gc-ed we break out of here
+	    if (!gc_eligible(&key, &to)) {
+		break;
+	    }
+	    
+	    // any valid data in the entry?
+	    let mut entry = Entry::default();
+	    entry.merge_from_bytes(&value).unwrap();
+
+	    let index = entry.get_index();
+	    let term = entry.get_term();
+	    let data = entry.get_data();
+	    // do I need datasize? see apply.rs:1104
+
+	    if !data.is_empty() {
+		let mut cmd = RaftCmdRequest::default();
+		cmd.merge_from_bytes(entry.data()).unwrap_or_else(|e| {
+		    panic!("log data unexpected at pos {}: {:?}",
+			   iter.position(), e);
+		});
+		if cmd.has_admin_request() {
+		    raft_wb.delete(&key)?;
+		    // we don't need to re-write this to the log
+		} else {
+		    let requests = req.get_requests();
+		    // we only need to do something if there is a Put
+		    // if there is at least one Put, we need to re-write
+		    // this entry and update key(s) in kv-lsm
+		    for req in requests {
+			let cmd_type = req.get_cmd_type();
+			match cmd_type {
+			    CmdType::Put => {
+				validated = 1;
+				raft_wb.delete(&key)?;
+				// we are going to have to do a manual write to wotr rather than re-insert the key through raft-lsm because of concurrency issues. Basically, it is very hard to know we get the right offset back from rocksdb. Wotr::WotrWrite() does have a lock.
+				kv_wb.put(...)?;
+			    },
+			    _ => continue,
+			}?;
+		    }
+		}
+	    }
+
+	    let _ = iter.next();
+	    new_vtail = iter.position();
+	}
+
+	self.consume(...); // write the new set of entries to loghead
+	self.put_cf(CF_DEFAULT, new_vtail);
+
+	Ok(total as usize)
+	
+//         if from == 0 {
+//             let start_key = keys::raft_log_key(raft_group_id, 0);
+//             let prefix = keys::raft_log_prefix(raft_group_id);
+//             match self.seek(&start_key)? {
+//                 Some((k, _)) if k.starts_with(&prefix) => from = box_try!(keys::raft_log_index(&k)),
+//                 // No need to gc.
+//                 _ => return Ok(0),
+//             }
+//         }
+//         if from >= to {
+//             return Ok(0);
+//         }
+
+//         for idx in from..to {
+//             let key = keys::raft_log_key(raft_group_id, idx);
+//             raft_wb.delete(&key)?;
+
+// 	    // avoid this!
+// //	    let entry: Option<Entry> = self.get_msg_cf_valuelog(CF_DEFAULT, &key);
+// //	    if Some(entry) {
+// 		// iterate through the entry, finding all Put keys
+// 		// query kv-lsm with each Put key
+// 		// - if key exists with same offset in kv-lsm:
+// 		//   - raft_wb.put_external(&key)
+// 		//   - kv_wb.put(new offset of updated key)
+// 		//   - fallocate depends on value of persisted v_tail (a new value which must be stored in kv-lsm)
+// 		// - if not:
+// 		//   - raft_wb.delete(&key)
+// 	    }
+	    
+//             if raft_wb.count() >= Self::WRITE_BATCH_MAX_KEYS * 2 {
+//                 raft_wb.write()?;
+//                 raft_wb.clear();
+//             }
+//         }
+//         Ok((to - from) as usize)
     }
 }
 
