82722cb2a8a0fc389371ca7ca0ba680249c3ec04
shawgerj <shawgerj@cs.wisc.edu>
2025-09-04 15:33:46 +0000

    force plain file cf snapshot, use wotr for snapshot iteration

--- components/raftstore/src/store/snap/io.rs
+++ components/raftstore/src/store/snap/io.rs
@@ -72,7 +72,7 @@ where
     };
 
     let mut stats = BuildStatistics::default();
-    box_try!(snap.scan_cf(cf, start_key, end_key, false, false, |key, value| {
+    box_try!(snap.scan_cf(cf, start_key, end_key, false, true, |key, value| {
         stats.key_count += 1;
         stats.total_size += key.len() + value.len();
         box_try!(BytesEncoder::encode_compact_bytes(&mut writer, key));
@@ -114,7 +114,7 @@ where
     let mut sst_writer = create_sst_file_writer::<E>(engine, cf, path)?;
     let mut stats = BuildStatistics::default();
     let mut remained_quota = 0;
-    // shawgerj surely this is broken but I don't think we are evaluating this
+
     box_try!(snap.scan_cf(cf, start_key, end_key, false, false, |key, value| {
         let entry_len = key.len() + value.len();
         while entry_len > remained_quota {
@@ -165,9 +165,13 @@ where
     };
 
     let mut wb = db.write_batch();
+    let mut opts = WriteOptions::default();
+    opts.set_sync(false);
+    opts.set_disable_wal(true);
+    
     let mut write_to_db = |batch: &mut Vec<(Vec<u8>, Vec<u8>)>| -> Result<(), EngineError> {
         batch.iter().try_for_each(|(k, v)| wb.put_cf(cf, k, v))?;
-        wb.write()?;
+        let _ = wb.write_valuelog(&opts)?;
         wb.clear();
         callback(batch);
         batch.clear();
