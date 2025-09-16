7d949fbbdc0407fa1fe4de2036d24ec4e87d3628
John Shawger <shawgerj@cs.wisc.edu>
2025-09-15 20:47:36 -0500

    slightly better error handling I hope

--- components/raftstore/src/store/peer_storage.rs
+++ components/raftstore/src/store/peer_storage.rs
@@ -503,7 +503,7 @@ fn init_raft_state<EK: KvEngine, ER: RaftEngine>(
         raft_state.last_index = RAFT_INIT_LOG_INDEX;
         raft_state.mut_hard_state().set_term(RAFT_INIT_LOG_TERM);
         raft_state.mut_hard_state().set_commit(RAFT_INIT_LOG_INDEX);
-	info!("writing init raft state");
+	info!("writing init raft state id {}", region.get_id());
         engines.raft.put_raft_state(region.get_id(), &raft_state)?;
     }
     info!("returning init raft state");
